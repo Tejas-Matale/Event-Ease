e7ea2104778b4372c0bd58b67185800f1eeae6c5
Tejas-Matale <tejasmatale99@gmail.com>
2024-07-10 13:50:26 +0530

    commit

--- frontend/src/app/components/partials/text-input/text-input.component.ts
+++ frontend/src/app/components/partials/text-input/text-input.component.ts
@@ -4,24 +4,22 @@ import { AbstractControl, FormControl } from '@angular/forms';
 @Component({
   selector: 'text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
 })
 export class TextInputComponent implements OnInit {
-@Input()
-control!:AbstractControl;
-@Input()
-showErrorsWhen:boolean = true;
-@Input()
-label!: string;
-@Input()
-type: 'text' | 'password' | 'email' = 'text';
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorsWhen: boolean = true;
+  @Input()
+  label!: string;
+  @Input()
+  type: 'text' | 'password' | 'email' = 'text';
 
-get formControl(){
-  return this.control as FormControl;
-}
-  constructor() { }
-
-  ngOnInit(): void {
+  get formControl() {
+    return this.control as FormControl;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
